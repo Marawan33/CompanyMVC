d9d17e1387f629cd580a866f77d551f54a6f1133
Marawan33 <marawan.mohamed112233@gmail.com>
2024-09-17 19:43:18 +0300

    Part 07 Fist Migration

--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -1,3 +1,6 @@
+using Company.Data.Contexts;
+using Microsoft.EntityFrameworkCore;
+
 namespace Company.Web
 {
     public class Program
@@ -8,6 +11,10 @@ namespace Company.Web
             //initiate Dev
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<CompanyDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
