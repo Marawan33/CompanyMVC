461fe3e1eec5e20fc7e50835464360f4f1f53c82
Marawan33 <marawan.mohamed112233@gmail.com>
2024-09-24 21:07:04 +0300

    Part 05 Department Controller With Service Layer

--- Company.Web/Program.cs
+++ Company.Web/Program.cs
@@ -2,6 +2,8 @@ using Company.Data.Contexts;
 using Company.Data.Models;
 using Company.Repository.Interfaces;
 using Company.Repository.Repositories;
+using Company.Service.Interfaces;
+using Company.Service.Services;
 using Microsoft.EntityFrameworkCore;
 
 namespace Company.Web
@@ -19,7 +21,9 @@ namespace Company.Web
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
             });
             builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+            
             builder.Services.AddScoped<IDepartmentRepository, DepartmentRepository>();
+            builder.Services.AddScoped<IDepartmentService, DepartmentService>();
             builder.Services.AddScoped<IGenericRepository<Employee>, GenericRepository<Employee>>();
             builder.Services.AddScoped<IGenericRepository<Department>, GenericRepository<Department>>();
 
