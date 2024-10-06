91a92e874b44780a3843040396355a24e58b4c29
Marawan33 <marawan.mohamed112233@gmail.com>
2024-10-07 01:44:02 +0300

    Part 05 Employee Create (Action & View) - ViewBag-ViewData-TempData

--- Company.Web/Controllers/EmployeeController.cs
+++ Company.Web/Controllers/EmployeeController.cs
@@ -1,4 +1,6 @@
-﻿using Company.Service.Interfaces;
+﻿using Company.Data.Models;
+using Company.Service.Interfaces;
+using Company.Service.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Company.Web.Controllers
@@ -6,10 +8,12 @@ namespace Company.Web.Controllers
     public class EmployeeController : Controller
     {
         private readonly IEmployeeService _employeeService;
+        private readonly IDepartmentService _departmentService;
 
-        public EmployeeController(IEmployeeService employeeService)
+        public EmployeeController(IEmployeeService employeeService,IDepartmentService departmentService)
         {
             _employeeService = employeeService;
+            _departmentService = departmentService;
         }
         public IActionResult Index(string searchInp)
         {
@@ -24,5 +28,30 @@ namespace Company.Web.Controllers
                 return View(emp);
             }
         }
-    }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            ViewBag.Department = _departmentService.GetAll();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Employee employee)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    _employeeService.Add(employee);
+                    return RedirectToAction(nameof(Index));
+                }
+                ModelState.AddModelError("DepartmentError", "ValidationError");
+                return View(employee);
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError("DepartmentError", ex.Message);
+                return View(employee);
+            }
+        }
+        }
 }
